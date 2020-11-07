c7fad9e95f9384d6fc68e198ab5b10ee827a7968
Chizzard74 <halpern74@protonmail.com>
2020-11-06 21:45:56 -0800

    updated credit

---
+++ greenfoot/first_try/MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        setBackground("images/scratch_pic.jpg");
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        butter_fly butter_fly = new butter_fly();
+        addObject(butter_fly,51,81);
+        butter_fly butter_fly2 = new butter_fly();
+        addObject(butter_fly2,125,117);
+    }
+}
